116b2315753ad9a21a06be33c1f924fd99aeac16
Jason Decastro <jasonrdecastro@gmail.com>
2016-09-30 14:31:47 -0400

    created a component where the frame of the wikipedia article will be dynamically rendered

---
+++ src/components/Frame.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Frame extends Component {
+  render() {
+    return (
+      <div className="col-md-9 col-sm-9 col-xs-12 main-div">
+        <input id="pageURL" type="text" className="form-control" value="" />
+        <iframe src={this.props.src} id="mainFrame"  />
+      </div>
+    )
+  }
+}
+
+module.exports = Frame
