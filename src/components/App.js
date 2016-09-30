b840dfad1bc4f027f75bcd62199e1c3b4328ea91
Jason Decastro <jasonrdecastro@gmail.com>
2016-09-30 14:32:55 -0400

    startign working on the logic of the lifecycle in react and how to best approach the fetching of random Wikipedia articles for the purpose of dynamically rendering them in the frame component

--- src/components/App.js
+++ src/components/App.js
@@ -1,63 +1,51 @@
 import React, { Component } from 'react';
 import '../static/App.css';
 import '../static/bootstrap.min.css';
+import Frame from './Frame';
+import Footer from './Footer';
+import Buttons from './Buttons';
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.articleLinkParser = this.articleLinkParser.bind(this)
+    this.state = {
+      articles: []
+    }
+  }
+
+  getArticles() {
+    const self = this
+    const url = 'https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=json&origin=*'
+    
+    fetch(url).then(response => {
+      return response.json()
+    }).then(articles => {
+      self.setState({articles: articles.query.random})
+    })
+    
+    debugger
+  }
+
+  componentWillMount() {
+    this.getArticles()
+  }
+
+  articleLinkParser() {
+    return this.state.articles[0].title.replace(/ /g, "_")
+  }
+
   render() {
     return (
       <div className="App">
-        <nav className="navbar navbar-default navbar-fixed-top visible-xs">
-          <div className="container">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <a className="navbar-brand" href="#"><strong>WIKI</strong><span>Roulette</span></a>
-              <button type="button" className="navbar-toggle collapsed nav-forward btn-primary btn-nav-forward">
-                <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-              </button>
-              <button type="button" className="navbar-toggle collapsed nav-back btn-nav-back">
-                <span className="glyphicon glyphicon-chevron-left" aria-hidden="true" disabled="disabled" />
-              </button>
-              </div>
-              <div id="navbar" className="navbar-collapse collapse">
-              
-        
-                <ul className="nav navbar-nav navbar-right">
-                  <li><a href="about.html">About</a></li>
-                </ul>
-              </div>
-            </div>
-        </nav>
-
-
         <div className="container">
           <div className="row">  		
-            <div className="col-md-9 col-sm-9 col-xs-12 main-div">
-              <input id="pageURL" type="text" className="form-control" value="" />
-              <iframe src="" id="mainFrame" frameborder="0" />
-            </div>
+            <Frame src={this.articleLinkParser} />
             <div className="col-xs-3 visible-sm visible-md visible-lg sidebar">
               <div className="logo"><h1>Wiki<br/><small>Roulette</small></h1></div>
-              <button className="btn btn-block btn-primary nav-forward">
-                NEXT <span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
-              </button>
-              <button className="btn btn-block btn-default btn-sm nav-back" disabled="disabled">
-                PREVIOUS
-              </button><hr/>
-              <a href="" id="newTab" target="_blank" className="btn btn-sm btn-block btn-default btn-alt">View on Wikipedia</a>
-              
-              <div className="well">
-                <p>
-                <a href="#"><strong>Install the Chrome Extension<br/></strong></a>
-                <a href="http://wikiroulette.co/about.html">About WikiRoulette</a></p>
-                <p>Use the <code>left</code> and <code>right</code> arrows to move forward and back</p>	
-                <p>Made with ♥ at <a target="_blank" href="http://learn.co/jasondecastro">Flatiron School</a></p>
-              </div>
+              <Buttons />
+              <Footer />
             </div>
           </div>
         </div>
