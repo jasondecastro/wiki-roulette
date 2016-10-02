82ace3888ab4b6e15cdb5e7c32071a07229270b3
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-02 01:08:01 -0400

    feature: implemented react-router for the about page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
+
+import App from './components/App'
+import About from './components/About'
+
+import './index.css'
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="about" component={About} />
+  </Router>,
   document.getElementById('root')
-);
+)
