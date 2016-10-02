213bb2bde49c2905d7b82480dad5d53227207bb0
Jason Decastro <jasonrdecastro@gmail.com>
2016-10-02 00:41:38 -0400

    refactor: edited the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div className="well">       
+      <div className="well">
+        <p>
+        <a target="_blank" href="https://chrome.google.com/webstore/detail/wikiroulette/fhahkjoaokdmndcggfbcfgppjnndaech"><strong>Install the Chrome Extension<br/></strong></a>
+        <a href="/about.html">About WikiRoulette</a></p>
         <p>Use the <code>left</code> and <code>right</code> arrows to move forward and back</p>	
-        <p>Made with ♥ by <a target="_blank" href="https://twitter.com/jasonrdecastro">Jason Decastro</a></p>
+        <p>Made by <a target="_blank" href="https://twitter.com/jasonrdecastro">@jasonrdecastro</a><br />with ♥ at <a target="_blank" href="http://learn.co/jasondecastro">The Flatiron School</a></p>
       </div>
     )
   }
